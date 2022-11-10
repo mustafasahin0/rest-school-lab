ca3469abbeea1e9d2b74ce37beee203265178c17
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-11-09 18:24:35 -0700

    Initial commit

---
+++ src/main/java/com/cydeo/dto/AddressDTO.java
@@ -0,0 +1,40 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.AddressType;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer", "teacher"}, ignoreUnknown = true)
+public class AddressDTO {
+
+    @JsonIgnore
+    private Long id;
+
+    private String street;
+    private String country;
+    private String state;
+    private String city;
+    private String postalCode;
+
+    private AddressType addressType;
+
+    @JsonBackReference(value = "student-reference")
+    private StudentDTO student;
+
+    @JsonIgnore
+    private ParentDTO parent;
+
+    private TeacherDTO teacher;
+
+    private Integer currentTemperature;
+
+}
